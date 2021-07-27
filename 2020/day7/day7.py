aea08ed7c97ca03f272043324a8311aa0a8ceda3
Waldemar <paninwaldemar@gmail.com>
2021-07-27 21:23:58 +0200

    day7 part2 done

--- 2020/day7/day7.py
+++ 2020/day7/day7.py
@@ -1,6 +1,6 @@
 input = open('input.txt').read().split("\n")
 input.remove('')
-bags, valid_bags = [], []
+bags, valid_bags, has_to_carry = [], [], []
 counter = 0
 
 
@@ -8,6 +8,11 @@ class Bag:
     def __init__(self, name, can_contain):
         self.name = name
         self.can_contain = can_contain
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.name == other.name
 
     def to_string(self):
         print(self.name)
@@ -41,3 +46,41 @@ while made_changes:
                     valid_bags.append(bag)
                     made_changes = True
 print(len(valid_bags))
+
+bags = []
+
+# Part 2
+print('Part 2: ')
+
+for rule in input:
+    rule = rule.split('contain')
+    main_bag = rule[0].replace('bags ', 'bag')
+    sec_bags = rule[1].split(',')
+
+    for i in range(len(sec_bags)):
+        sec_bags[i] = Bag(sec_bags[i].replace('bags', 'bag').replace('.', '')[1:], [])
+
+        if sec_bags[i] in bags:
+            sec_bags[i] = Bag(sec_bags[i], sec_bags[i].can_contain)
+        else:
+            sec_bags[i] = Bag(sec_bags[i], [])
+
+    if main_bag == 'shiny gold bag':
+        curr_bag = Bag(main_bag, sec_bags)
+        has_to_carry.append(curr_bag)
+    curr_bag = Bag(main_bag, sec_bags)
+    bags.append(curr_bag)
+
+
+made_changes = True
+while made_changes:
+    made_changes = False
+    for a in has_to_carry:
+        print type(a)
+    for bag in bags:
+        if bag in has_to_carry:
+            for a in bag.can_contain:
+                if a not in has_to_carry:
+                    has_to_carry.append(a)
+                    made_changes = True
+
