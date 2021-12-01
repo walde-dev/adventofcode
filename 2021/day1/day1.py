70f4281aa2f9ad50f880ae5afb70ae8320e79865
walde-dev <paninwaldemar@gmail.com>
2021-12-01 13:29:03 +0100

    day1 part2

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,20 +1,39 @@
-from typing import Counter
-
-
 with open('input.txt') as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
     lines = [int(line) for line in lines]
 
-currDepth,  counter,  temp = 0, 0, 0
 
+def count_increments(list):
+    counter,  temp = 0, 0
+    for i, line in enumerate(list):
+        if i == 0:
+            temp = line
+            continue
+        if line > temp:
+            counter += 1
+        temp = line
+    return counter
+
+
+# part1
+print(count_increments(lines))
+
+# part2
+temp1, temp2, total = 0, 0, 0
+windows = []
 
 for i, line in enumerate(lines):
-    if i==0:
-        temp = line
+    if i == 0:
+        temp1 = line
         continue
-    if line>temp:
-        counter+=1
-    temp = line
+    if i == 1:
+        temp2 = temp1
+        temp1 = line
+        continue
+    total = temp1+temp2+line
+    windows.append(total)
+    temp2 = temp1
+    temp1 = line
 
-print(counter)
+print(count_increments(windows))
