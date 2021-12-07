fce82d9a252d78b9e9ce24d229f06f40a8b1e667
walde-dev <paninwaldemar@gmail.com>
2021-12-07 04:29:45 +0100

    day3 part2 done

--- 2021/day3/day3.py
+++ 2021/day3/day3.py
@@ -20,9 +20,83 @@ for idx, digit in enumerate(gamma):
         gamma[idx] = 0
         epsilon[idx] = 1
 
+pos = 0
+while pos < 12:
+    if(len(lines) == 1):
+        break
+    # get highest occuring bit in pos
+    toRemove = []
+    counter = 0
+    for line in lines:
+        if int(line[pos]) == 0:
+            counter = counter - 1
+        if int(line[pos]) == 1:
+            counter = counter + 1
 
+    # 1 is highest occurent at pos
+    if counter >= 0:
+        for line in lines:
+            if int(line[pos]) == 0:
+                toRemove.append(line)
+
+    # 0 is highest occurent at pos
+    if counter < 0:
+        for line in lines:
+            if int(line[pos]) == 1:
+                toRemove.append(line)
+
+    for removal in toRemove:
+        if removal in lines:
+            lines.remove(removal)
+    counter = 0
+    pos = pos + 1
+
+oxygen = lines
+
+with open('input.txt') as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+pos = 0
+while pos < 12:
+    if(len(lines) == 1):
+        break
+    # get highest occuring bit in pos
+    toRemove = []
+    counter = 0
+    for line in lines:
+        if int(line[pos]) == 0:
+            counter = counter - 1
+        if int(line[pos]) == 1:
+            counter = counter + 1
+
+    # 1 is highest occurent at pos
+    if counter >= 0:
+        for line in lines:
+            if int(line[pos]) == 1:
+                toRemove.append(line)
+
+    # 0 is highest occurent at pos
+    if counter < 0:
+        for line in lines:
+            if int(line[pos]) == 0:
+                toRemove.append(line)
+
+    for removal in toRemove:
+        if removal in lines:
+            lines.remove(removal)
+    counter = 0
+    pos = pos + 1
+
+scrubber = lines
+
+print(oxygen)
+
+oxygen = int(''.join(map(str, oxygen)), base=2)
+scrubber = int(''.join(map(str, scrubber)), base=2)
 gamma = int(''.join(map(str, gamma)), base=2)
 epsilon = int(''.join(map(str, epsilon)), base=2)
 
-
 print('Part 1:', gamma, 'x', epsilon, '=', gamma*epsilon)
+print('Part 2:', oxygen, 'x', scrubber, '=', oxygen*scrubber)
+
